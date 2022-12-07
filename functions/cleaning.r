398cee53ced79a29d91e1bf4ad754175f9810139
1042995cedars <117742571+1042995cedars@users.noreply.github.com>
2022-12-07 13:33:39 +0000

    Add files via upload

---
+++ functions/cleaning.r
@@ -0,0 +1,13 @@
+cleaning <- function(data_raw){
+  data_raw %>%
+    clean_names() %>%
+    remove_empty(c("rows", "cols")) %>%
+    select(-starts_with("delta")) %>%
+    select(-comments)
+}
+
+remove_empty_flipper_length <- function(data_clean){
+  data_clean %>%
+    filter(!is.na(flipper_length_mm)) %>%
+    select(species, flipper_length_mm)
+}
